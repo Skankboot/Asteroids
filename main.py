0f85d04381633864a4a89de3bcd9c99f3e313013
DLetang <dletang2@gmail.com>
2025-06-21 14:57:16 -0400

    added player rotation

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import pygame, constants, player
-from constants import *
+import pygame, constants, player # type: ignore
+from constants import * # type: ignore
 
 def main():
 
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # type: ignore
     my_guy = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
 
     fps_timer = pygame.time.Clock()
@@ -17,7 +17,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-
+        
+        my_guy.update(dt)
         screen.fill((0,0,0))
         my_guy.draw(screen)
         pygame.display.flip()
