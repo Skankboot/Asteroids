3aa9a12a004b5b5c3f3a7771ab8247b456d5746d
DLetang <dletang2@gmail.com>
2025-06-21 13:22:41 -0400

    added player initialization

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import pygame, constants
+import pygame, constants, player
 from constants import *
 
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    my_guy = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
+
+    fps_timer = pygame.time.Clock()
+    dt = 0
         
+
+    while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
 
         screen.fill((0,0,0))
+        my_guy.draw(screen)
+        pygame.display.flip()
 
-        screen.display.flip()
-
+        dt = (fps_timer.tick(60))/1000
+        
     
 
 if __name__ == "__main__":
